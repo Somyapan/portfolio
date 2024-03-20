ce74271f4ad821c750c13c76b3b5cb14fcbb7860
Somya Panchal <somyapancom@gmail>
2024-03-20 10:49:35 +0530

    portfolio created

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        poppins:'Poppins',
+        dynalight:'Dynalight',
+
+      },
+      colors:{
+        primary:'#a435f0',
+        secondary:'#0c0c0d',
+
+      }
+    },
+  },
+  plugins: [],
+}
+
